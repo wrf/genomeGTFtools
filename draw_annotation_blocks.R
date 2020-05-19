b041aba56469ed678460b39274db629c65f85538
wrf <wfrancis@biology.sdu.dk>
2020-05-19 09:25:57 +0100

    fix to draw strandless features

--- draw_annotation_blocks.R
+++ draw_annotation_blocks.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 # convert tabular annotation format to rectangle blocks
 # or polygon blocks for single CDS genes, like bacteria
-# last modified 2020-05-18
+# last modified 2020-05-19
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -44,6 +44,8 @@ is_forward = which(mrnatypes[,5]=="+")
 forward_tx = mrnatypes[is_forward,]
 is_reverse = which(mrnatypes[,5]=="-")
 reverse_tx = mrnatypes[is_reverse,]
+is_strandless = which(mrnatypes[,5]==".")
+nostrand_tx = mrnatypes[is_strandless,]
 
 exontypes = annottab[which(annottab[,2]=="exon"),]
 # for cases where no mRNA is given, assume meant to be entire genes as blocks
@@ -58,12 +60,14 @@ print(paste("# counted", length(tx_names) ,"transcripts"))
 tx_index = match(mrnatypes[,1],tx_names)
 exon_index = match(exontypes[,1],tx_names)
 
-
+# draw the PDF
 pdf(file=outputfile, width=8, height=10)
 plot(0,0, type='n', axes=FALSE, frame.plot=FALSE, ylim=c(0,num_tx), xlim=c(window_start,window_end), xlab="", ylab="")
 par(mar=c(4.5,2,1,1))
 axis(1, cex.axis=1.4)
+# if drawing genes alone, no exons, then draw them as polygons
 if (draw_polygons==TRUE) {
+	# draw forward polygons
 	for (yval in tx_index[is_forward]) {
 		genelen = mrnatypes[yval,4]-mrnatypes[yval,3]
 		if (genelen < offset_width) {
@@ -75,6 +79,7 @@ if (draw_polygons==TRUE) {
 		forward_y = c( yval, yval-0.3, yval-0.3, yval+0.3, yval+0.3, yval)
 		polygon( forward_x, forward_y, col="#25893a")
 	}
+	# draw reverse polygons
 	for (yval in tx_index[is_reverse]) {
 		genelen = mrnatypes[yval,4]-mrnatypes[yval,3]
 		if (genelen < offset_width) {
@@ -86,14 +91,23 @@ if (draw_polygons==TRUE) {
 		reverse_y = c( yval, yval-0.3, yval-0.3, yval+0.3, yval+0.3, yval)
 		polygon( reverse_x, reverse_y, col="#25893a")
 	}
+	# draw rectangles for strandless features, usually this is an error
+	for (yval in tx_index[is_strandless]) {
+		rect(mrnatypes[yval,3], yval-0.3, mrnatypes[yval,4], yval+0.3, col="#25893a")
+	}
+# otherwise draw arrows marking direction, and each exon becomes a box
+# strandless exons should be drawn but have no arrow
 } else {
 	# draw as arrows and boxes
 	arrows(forward_tx[,3], tx_index[is_forward], forward_tx[,4]+offset_width, tx_index[is_forward], lwd=3, angle=15, length=0.1, col="#bbbbbb")
 	arrows(reverse_tx[,4], tx_index[is_reverse], reverse_tx[,3]-offset_width, tx_index[is_reverse], lwd=3, angle=15, length=0.1, col="#bbbbbb")
 	rect(exontypes[,3], exon_index-0.3, exontypes[,4], exon_index+0.3, col="#25893a")
 }
+# write the names of each transcript
 text(mrnatypes[,3], tx_index, tx_names, pos=2)
+# write the scaffold name on the margin
 mtext(axistype[1,1], side=1, at=window_start-(1.5*offset_width), cex=1.8, line=-0.4)
+#
 dev.off()
 
 
