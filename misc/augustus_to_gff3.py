9a181b7a9daeba6351a95a4c8059e24521458c2e
wrf <wfrancis@biology.sdu.dk>
2020-09-04 13:51:35 +0200

    allow format with exons

--- misc/augustus_to_gff3.py
+++ misc/augustus_to_gff3.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
 '''
-  augustus_to_gff3.py  last modified  2020-03-23
+  augustus_to_gff3.py  last modified  2020-09-03
   generate true GFF3 from augustus output
   involves creating mRNA features from transcript
   and exon from CDS
   also will formalize some ID-Parent tags
 
 augustus_to_gff3.py augustus.gff > augustus_no_comments.gff3
+
+  some formats include exons already, rather than needing to create them from CDS
+  use -x
+
+augustus_to_gff3.py braker2.gff -x > braker2_no_comments.gff3
 '''
 
 import sys
@@ -15,6 +20,11 @@ import sys
 if len(sys.argv) < 2:
 	sys.exit( __doc__ )
 else:
+	if len(sys.argv)==3 and sys.argv[2]=="-x":
+		exon_from_CDS = False
+	else:
+		exon_from_CDS = True
+
 	for line in open(sys.argv[1],'r'):
 		line = line.strip()
 		if line and line[0] != "#":
@@ -26,19 +36,23 @@ else:
 				newattrs = "ID={0};Name={0}".format(attributes)
 			elif feature=="transcript": # changed to ID and Name
 				lsplits[2] = "mRNA"
-				newattrs = "ID={0};Parent={1};Name={0}".format(attributes, attributes.split(".")[0])
+				attrd = dict([(field.strip().split(" ",1)) for field in attributes.split(";") if field])
+				if attrd.get("gene_id", False) and attrd.get("transcript_id", False):
+					newattrs = "ID={0};Parent={1}".format(attrd.get("transcript_id").replace('"',''), attrd.get("gene_id").replace('"',''))
+				else:
+					newattrs = "ID={0};Parent={1};Name={0}".format(attributes, attributes.split(".")[0])
 			elif feature=="CDS": # change to ID and Parent
-				attrd = dict([(field.strip().split(" ")) for field in attributes.split(";") if field])
+				attrd = dict([(field.strip().split(" ",1)) for field in attributes.split(";") if field])
 				transcriptid = attrd.get("transcript_id").replace('"','')
 				newattrs = "ID={0}.cds;Parent={0}".format(transcriptid)
 			elif feature=="stop_codon" or feature=="start_codon":
-				attrd = dict([(field.strip().split(" ")) for field in attributes.split(";") if field])
+				attrd = dict([(field.strip().split(" ",1)) for field in attributes.split(";") if field])
 				transcriptid = attrd.get("transcript_id").replace('"','')
 				newattrs = "Parent={0}".format(transcriptid)
 			else:
 				continue
 			lsplits[8] = newattrs
-			if feature=="CDS":
+			if feature=="CDS" and exon_from_CDS:
 				exonsplits = list(lsplits)
 				exonsplits[2] = "exon"
 				exonsplits[7] = "."
