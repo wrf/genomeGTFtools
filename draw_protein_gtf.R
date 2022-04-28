d1fab49eaae43ed8632ae1a3c260c88bcec22c58
wrf <wrfbiolum@protonmail.com>
2022-04-28 16:46:41 +0200

    fixed offset for cases with too many domains

--- draw_protein_gtf.R
+++ draw_protein_gtf.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 #
 # convert protein GTF format to rectangle blocks for domains
-# last modified 2022-03-15
+# last modified 2022-04-28
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -52,6 +52,11 @@ domainids = gsub("ID=([\\w\\d]+)\\.([\\w\\d-]+)\\..+","\\2",PFAMtypes[,9],perl=T
 maxend = max(gtftab[,5])
 protlenrange = pretty(c(0,maxend))
 
+# set offsets for block width
+offs_u = 8
+offs_l = 12
+offs_m = 10
+
 # OUTPUT
 pdf(outputfile, width=8, height=11, title = basename(inputfile) )
 par(mar=c(2.5,2.5,1,2) )
@@ -67,8 +72,8 @@ for (i in 1:length(countgroups)) {
   
   # get domain position information
   protindex = match(PFAMtypes[,1],protnames_per_page)
-  yupper = protindex*10-3
-  ylower = protindex*10-7
+  yupper = protindex*10 - offs_u
+  ylower = protindex*10 - offs_l
   domstart = PFAMtypes[,4]
   domend = PFAMtypes[,5]
   
@@ -78,15 +83,15 @@ for (i in 1:length(countgroups)) {
   
   # make black lines for length of the protein
   # this works even if no protein types are given, instead just does not draw lines
-  ymiddles = match(proteintypes[,1],protnames_per_page)*10-5
+  ymiddles = match(proteintypes[,1],protnames_per_page)*10 - offs_m
   linexcoords = c(rbind(proteintypes[,4],proteintypes[,5],NA,NA))
   lineycoords = c(rbind(ymiddles,ymiddles,NA,NA))
   lines(linexcoords,lineycoords, lwd=2)
   
   # make signal peptides as black boxes
   signalindex = match(signaltypes[,1],protnames_per_page)
-  sigupper = signalindex*10-3
-  siglower = signalindex*10-7
+  sigupper = signalindex*10 - offs_u
+  siglower = signalindex*10 - offs_l
   sigstart = signaltypes[,4]
   sigend = signaltypes[,5]
   rect(sigstart, siglower, sigend, sigupper, col="#000000")
@@ -95,7 +100,7 @@ for (i in 1:length(countgroups)) {
   #axis(1,at=protlenrange)
   rect(domstart, ylower, domend, yupper, col=colvec[domainindex])
   #text(domstart, protindex*10-1, domainids, pos=4, offset=0)
-  text(c(0),match(protnames_per_page,protnames_per_page)*10-1, 
+  text(c(0),match(protnames_per_page,protnames_per_page)*10 - (offs_u-2), 
        protnames_per_page, pos=4, offset=0)
   legend(0, yscale_max, legend=domainids[match(domnames,domaincodes)],
          col=colvec, pch=15, x.intersp=0.5, ncol=4, bty='o')
